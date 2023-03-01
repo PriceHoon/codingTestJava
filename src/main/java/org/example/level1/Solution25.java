7c2ac77b2441d6f0f2f51987a1c6ec8aefe09e8b
PriceHoon <saog3151@gmail.com>
2023-03-02 01:35:03 +0900

    짝수와 홀수

---
+++ src/main/java/org/example/level1/Solution25.java
@@ -0,0 +1,16 @@
+package org.example.level1;
+
+public class Solution25 {
+
+    public String solution(int num) {
+
+        //정수 num이 짝수일 경우 Even,홀수 Odd반환
+
+        if (num % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+
+    }
+}
